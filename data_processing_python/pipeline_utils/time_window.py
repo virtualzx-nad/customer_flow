d56ff1b50dbd4a5488d9f17d64c9f6881a6cbfa6
Xiaolei Zhu <virtualzx@gmail.com>
2019-09-21 23:03:12 -0700

    Add working scripts for moving count, hasing streams to different partitions, producer from S3 content, and schema transformation.

--- data_processing_python/pipeline_utils/time_window.py
+++ data_processing_python/pipeline_utils/time_window.py
@@ -19,13 +19,13 @@ def incr(state, key, data):
 def decr(state, key, data):
     state.decr(key)
 
-def output_integer(state, key):
+def output_integer(state, key, data):
     """Directly output the value associated with the key."""
     return int(state[key])
     
 def process_time_window(add_func, remove_func, init_func, topic, schema,
                         output_topic, output_func, output_field, output_schema,
-                        window=10.0, key_by=None, name=None,
+                        window=10.0, key_by=None, name=None, timeout=None,
                         broker='pulsar://localhost:6650', max_records=-1,
                         state_server='localhost', state_id=1,
                         date_field='date', date_format='%Y-%m-%d %H:%M:%S',
@@ -51,19 +51,29 @@ def process_time_window(add_func, remove_func, init_func, topic, schema,
     OutModel = model_class_factory(**output_schema)
     producer = client.create_producer(output_topic,
                                       schema=pulsar.schema.AvroSchema(OutModel))
+    if output_field not in output_schema:
+        raise KeyError('The output field is not in the schema.')
+    if key_by is not None and key_by not in schema:
+        raise KeyError('No field matched the `key_by` setting')
+    for key in output_schema:
+        if key == output_field:
+            continue
+        if key not in schema:
+            raise KeyError('Output schema contains unknown fields')
 
     t0 = time.time()
     i = 0
     while i != max_records:
-        i += 1
         try:
-            message = consumer.receive(1000)
+            message = consumer.receive(timeout)
         except Exception as e:
             logger.info('Consumer has been depleted.  Message %s', str(e))
-            t0 += 1
+            t0 += timeout * 1e-3
             break
         data = message.value().__dict__
         consumer.acknowledge(message)
+        i += 1
+
         key = "___all-fields___" if key_by is None else data[key_by]
         t_right = datetime.datetime.strptime(data[date_field], date_format).timestamp()
         # First time a key is encountered
@@ -75,7 +85,7 @@ def process_time_window(add_func, remove_func, init_func, topic, schema,
             add_func(state, key, data)
         else:
             init_func(state, key, data)
-        keys_changed = {key}
+        keys_changed = {key: data}
         key_decreased = False
         while t_left <= t_right - window and reader.has_message_available():
             message = reader.read_next()
@@ -83,17 +93,16 @@ def process_time_window(add_func, remove_func, init_func, topic, schema,
             keyl = '___all-fields___' if key_by is None else data[key_by]
             t_left = datetime.datetime.strptime(data[date_field], date_format).timestamp()
             remove_func(state, keyl, data)
-            keys_changed.add(keyl)
-        for key in keys_changed:
-            output = output_func(state, key)
+            keys_changed[keyl] = data
+        for key, data in keys_changed.items():
+            output = output_func(state, key, data)
             if output is None:
                 continue
-            if key_by is None:
-                producer.send(OutModel.from_dict({output_field: output}))
-            else:
-                producer.send(OutModel.from_dict({key_by: key, output_field:output}))
-
-    logger.info('Average processing rate: {:.2f} records/s'.format(i/(time.time()-t0)))
+            record = {field: output if field == output_field else data[field]
+                      for field in output_schema}
+            producer.send(OutModel.from_dict(record))
+    logger.info('Total messages processsed: %d', i)
+    logger.info('Average processing rate: %.2f records/s', i/(time.time()-t0))
     client.close()
 
 
---
+++ data_processing_python/scripts/hash_stream.py
@@ -0,0 +1,56 @@
+"""Hash a message stream into multiple streams based on a key.
+(Equivalent to Flink KeyBy)"""
+import logging
+import sys
+import time
+
+import pulsar
+import yaml
+
+from pipeline_utils.schema_model import model_class_factory
+
+
+if len(sys.argv) < 1:
+    raise ArgumentError('Did not suppy settings through yaml file')
+with open(sys.argv[1]) as f:
+    settings = yaml.safe_load(f)
+
+logging.basicConfig(level=settings.get('logging_level', 'INFO'))
+logger = logging.getLogger(__name__)
+
+# Connect the consumer, i.e. stream to be splitted.
+client = pulsar.Client(settings['broker'])
+Model = model_class_factory(**settings['schema'])
+avro_schema = pulsar.schema.AvroSchema(Model)
+consumer = client.subscribe(settings['topic'], schema=avro_schema,
+                            subscription_name=settings['name'])
+
+# Connect the producers for each substream
+partitions = settings['partitions']
+producers = [client.create_producer(settings['topic_format'].format(index),
+                                    schema=avro_schema)
+             for index in range(partitions)]
+
+# Start hashing streams!!!!
+max_records = settings['max_records']
+key_by = settings['key_by']
+timeout = settings['timeout']
+t0 = time.time()
+i = 0
+while i != max_records:
+    i += 1
+    try:
+        message = consumer.receive(timeout)
+    except Exception as e: 
+        logger.info('Source stream timeout. '+ str(e))
+        t0 += timeout * 0.001
+        break
+    data = message.value()
+    consumer.acknowledge(message)
+    key = getattr(data, key_by)
+    index = hash(key) % partitions
+    producers[index].send(data)
+logger.info('Total messages processed: %d', i)
+logger.info('Average processing rate: %.2f records/s', i/(time.time()-t0))
+client.close()
+
