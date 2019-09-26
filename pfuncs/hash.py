3eb79b0633d3d573b0d9c83c61e8c8c2286667f9
Xiaolei Zhu <virtualzx@gmail.com>
2019-09-26 14:00:39 -0700

    Pulsar functions tests

---
+++ pfuncs/hash.py
@@ -0,0 +1,20 @@
+from pulsar import Function
+from pulsar.schema import AvroSchema
+
+from pipeline_utils import model_class_factory
+
+
+class HashStream(Function):
+    def process(self, input, context):
+        config = context.user_config
+        RecordClass = model_class_factory(**config['schema'])
+        record = AvroSchema(RecordClass).decode(input.encode('utf-8'))
+        key_by = config['key_by']
+        partitions = config['partitions']
+        shift = config['shift']
+        format_str = config['topic_format']
+        key = getattr(record, key_by)
+        index = hash(key) % partitions
+        out_topic = format_str.format(index + shift)
+        context.publish(out_topic, input)
+        return
