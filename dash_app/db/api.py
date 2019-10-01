cb84d5bdd78656cd07e52185c84b98e80f8f46fe
Xiaolei Zhu <virtualzx@gmail.com>
2019-10-01 00:43:26 +0000

    Update API to include real-time occupancy data.

--- dash_app/db/api.py
+++ dash_app/db/api.py
@@ -31,8 +31,16 @@ def get_categories():
     return {entry.decode().split(':')[1]: entry.decode()
             for entry in redis.smembers('catalog:categories')}
 
+def occupancy(business_id):
+    """Compute the occupancy of a business id"""
+    max_occ = redis.lindex('max:'+business_id, -1)
+    occ = redis.lindex('max:'+business_id, 0)
+    if max_occ is None:
+        return 0, 1
+    return occ, max_occ
 
 def get_info_near(longitude, latitude, radius, unit='km', max_results=10000, random=False, category='geo:Restaurants'):
+    """Retrieve businesses near a particular coordinate and their occupancy"""
     if random:
         nearby = redis.georadius(category, longitude, latitude, radius,
                                  unit=unit, withcoord=True)
@@ -46,17 +54,22 @@ def get_info_near(longitude, latitude, radius, unit='km', max_results=10000, ran
         else:
             nearby = nearby[:max_results]
 
-    ids, latitudes, longitudes = [], [], []
+    ids, latitudes, longitudes, ratio, max_occupancy = [], [], [], [], []
     for data, (lon, lat) in nearby:
         business_id, name = map(literal_eval, data.decode().split('||'))
         ids.append(name)
         longitudes.append(lon)
         latitudes.append(lat)
+        occ, max_occ = get_occupancy(business_id)
+        ratio.append(occ/max_occ)
+        max_occupancy.append(max_occ + 1)
 
     return dict(
             type="scattermapbox",
             lon=longitudes,
             lat=latitudes,
+            color=ratio,
+            size=max_occupancy,
             text=ids,
             name='nearby_business',
             marker=dict(size=3, opacity=0.6)
